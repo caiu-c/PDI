5c27ff4c0c5b504a8b8bfdd96efe2fd5e5a7b25d
Caiuc <contato.caiuc@gmail.com>
2026-01-20 11:12:36 -0300

    Adicionando regras ao .gitignore

--- main2.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-caminho = Path(r"C:\Users\caio_\Downloads\_DESESPERO_PDI3\fig")
-
-for arquivo in caminho.iterdir():
-    if arquivo.is_file():
-        print(arquivo.name)
